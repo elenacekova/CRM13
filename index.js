afe29ca086dbf1a96d61febc1f4e2abe24439f27
Elena <elenacekova>
2014-02-06 23:29:55 +0100

    a

---
+++ index.js
@@ -0,0 +1,28 @@
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+
+function onDeviceReady() {}
+
+
+function onLoad(){
+
+      $(document).ready(function(){
+      $("#second").hide();
+    
+
+    
+
+      // start of the function after login button click
+      $("#btn_ok").click(function(){
+
+      $("#first").hide();
+      $("#second").show();
+
+      }) // end of the function after login button click
+
+
+
+
+
+}
